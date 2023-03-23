49116b43b19a1ae05e73a08a8b1b5ba55bee3808
Irene Bandera <irenebandera@eprosima.com>
2023-03-23 15:26:10 +0100

    Finish simple tests
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

--- fastddsspy_participants/test/visualization/unittest/ModelParserTest.cpp
+++ fastddsspy_participants/test/visualization/unittest/ModelParserTest.cpp
@@ -39,12 +39,12 @@ TEST(ModelParserTest, simple_participant)
     result = spy::participants::ModelParser::participants(model);
 
     // Create expected return
-    std::vector<spy::participants::SimpleParticipantData> desire_result;
-    desire_result.push_back({"hello", guid_data});
+    std::vector<spy::participants::SimpleParticipantData> expected_result;
+    expected_result.push_back({"hello", guid_data});
 
     // Check information
-    ASSERT_EQ(result[0].name, desire_result[0].name);
-    ASSERT_EQ(result[0].guid, desire_result[0].guid);
+    ASSERT_EQ(result[0].name, expected_result[0].name);
+    ASSERT_EQ(result[0].guid, expected_result[0].guid);
 }
 
 TEST(ModelParserTest, simple_participant_two_participants)
@@ -72,19 +72,197 @@ TEST(ModelParserTest, simple_participant_two_participants)
     result = spy::participants::ModelParser::participants(model);
 
     // Create expected return
-    std::vector<spy::participants::SimpleParticipantData> desire_result;
-    desire_result.push_back({"hello1", guid_data1});
-    desire_result.push_back({"hello2", guid_data2});
+    std::vector<spy::participants::SimpleParticipantData> expected_result;
+    expected_result.push_back({"hello1", guid_data1});
+    expected_result.push_back({"hello2", guid_data2});
 
     // Check information
     unsigned int i = 0;
     for (const auto& participant : model.participant_database_)
     {
-        ASSERT_EQ(result[i].name, desire_result[i].name);
-        ASSERT_EQ(result[i].guid, desire_result[i].guid);
+        ASSERT_EQ(result[i].name, expected_result[i].name);
+        ASSERT_EQ(result[i].guid, expected_result[i].guid);
         i++;
     }
 
+}
+
+TEST(ModelParserTest, simple_endpoint_writer)
+{
+    // Create model
+    spy::participants::SpyModel model;
+    // Fill model
+    ddspipe::core::types::Guid guid_data = ddspipe::core::testing::random_guid();
+    ddspipe::core::types::DdsTopic topic = ddspipe::core::testing::random_dds_topic();
+    spy::participants::EndpointInfo endpoint = {
+                                                    ddspipe::core::types::EndpointKind::writer,
+                                                    guid_data,
+                                                    topic
+
+                                                };
+    model.endpoint_database_.add(guid_data, endpoint);
+
+    // Obtain information from model
+    std::vector<spy::participants::SimpleEndpointData> result;
+    result = spy::participants::ModelParser::writers(model);
+
+    // Create expected return
+    std::vector<spy::participants::SimpleEndpointData> expected_result;
+    expected_result.push_back({
+                                guid_data,
+                                "participant_name",
+                                {
+                                    topic.m_topic_name,
+                                    topic.type_name
+                                }
+
+                            });
+
+    // Check information
+    ASSERT_EQ(result[0].guid, expected_result[0].guid);
+    ASSERT_EQ(result[0].topic.topic_name, expected_result[0].topic.topic_name);
+    ASSERT_EQ(result[0].topic.topic_type, expected_result[0].topic.topic_type);
+}
+
+TEST(ModelParserTest, simple_endpoint_reader)
+{
+    // Create model
+    spy::participants::SpyModel model;
+    // Fill model
+    ddspipe::core::types::Guid guid_data = ddspipe::core::testing::random_guid();
+    ddspipe::core::types::DdsTopic topic = ddspipe::core::testing::random_dds_topic();
+    spy::participants::EndpointInfo endpoint = {
+                                                    ddspipe::core::types::EndpointKind::reader,
+                                                    guid_data,
+                                                    topic
+
+                                                };
+    model.endpoint_database_.add(guid_data, endpoint);
+
+    // Obtain information from model
+    std::vector<spy::participants::SimpleEndpointData> result;
+    result = spy::participants::ModelParser::readers(model);
+
+    // Create expected return
+    std::vector<spy::participants::SimpleEndpointData> expected_result;
+    expected_result.push_back({
+                                guid_data,
+                                "participant_name",
+                                {
+                                    topic.m_topic_name,
+                                    topic.type_name
+                                }
+
+                            });
+
+    // Check information
+    ASSERT_EQ(result[0].guid, expected_result[0].guid);
+    ASSERT_EQ(result[0].topic.topic_name, expected_result[0].topic.topic_name);
+    ASSERT_EQ(result[0].topic.topic_type, expected_result[0].topic.topic_type);
+}
+
+TEST(ModelParserTest, simple_endpoint_writer_readers)
+{
+    // Create model
+    spy::participants::SpyModel model;
+    // Fill model
+    ddspipe::core::types::Guid guid_data = ddspipe::core::testing::random_guid();
+    ddspipe::core::types::DdsTopic topic = ddspipe::core::testing::random_dds_topic();
+    spy::participants::EndpointInfo endpoint = {
+                                                    ddspipe::core::types::EndpointKind::writer,
+                                                    guid_data,
+                                                    topic
+
+                                                };
+    model.endpoint_database_.add(guid_data, endpoint);
+
+    // Obtain information from model
+    std::vector<spy::participants::SimpleEndpointData> result;
+    result = spy::participants::ModelParser::readers(model);
+
+    // Check information
+    ASSERT_EQ(result.size(), 0);
+    ASSERT_EQ(model.endpoint_database_.size(), 1);
+}
+
+TEST(ModelParserTest, simple_endpoint_reader_writers)
+{
+    // Create model
+    spy::participants::SpyModel model;
+    // Fill model
+    ddspipe::core::types::Guid guid_data = ddspipe::core::testing::random_guid();
+    ddspipe::core::types::DdsTopic topic = ddspipe::core::testing::random_dds_topic();
+    spy::participants::EndpointInfo endpoint = {
+                                                    ddspipe::core::types::EndpointKind::reader,
+                                                    guid_data,
+                                                    topic
+
+                                                };
+    model.endpoint_database_.add(guid_data, endpoint);
+
+    // Obtain information from model
+    std::vector<spy::participants::SimpleEndpointData> result;
+    result = spy::participants::ModelParser::writers(model);
+
+    // Check information
+    ASSERT_EQ(result.size(), 0);
+    ASSERT_EQ(model.endpoint_database_.size(), 1);
+}
+
+TEST(ModelParserTest, simple_topic)
+{
+    // Create model
+    spy::participants::SpyModel model;
+    // Fill model
+
+    ddspipe::core::types::DdsTopic topic = ddspipe::core::testing::random_dds_topic();
+    ddspipe::core::types::Guid guid_data_writer_1 = ddspipe::core::testing::random_guid();
+    spy::participants::EndpointInfo endpoint_writer_1 = {
+                                                        ddspipe::core::types::EndpointKind::writer,
+                                                        guid_data_writer_1,
+                                                        topic
+
+                                                    };
+    model.endpoint_database_.add(guid_data_writer_1, endpoint_writer_1);
+    ddspipe::core::types::Guid guid_data_writer_2 = ddspipe::core::testing::random_guid(2);
+    spy::participants::EndpointInfo endpoint_writer_2 = {
+                                                        ddspipe::core::types::EndpointKind::writer,
+                                                        guid_data_writer_2,
+                                                        topic
+
+                                                    };
+    model.endpoint_database_.add(guid_data_writer_2, endpoint_writer_2);
+    ddspipe::core::types::Guid guid_data_reader_1 = ddspipe::core::testing::random_guid(3);
+    spy::participants::EndpointInfo endpoint_reader_1 = {
+                                                        ddspipe::core::types::EndpointKind::reader,
+                                                        guid_data_reader_1,
+                                                        topic
+
+                                                    };
+    model.endpoint_database_.add(guid_data_reader_1, endpoint_reader_1);
+
+    // Obtain information from model
+    std::vector<spy::participants::SimpleTopicData> result;
+    result = spy::participants::ModelParser::topics(model);
+
+    // Create expected return
+    std::vector<spy::participants::SimpleTopicData> expected_result;
+    expected_result.push_back({
+                                topic.m_topic_name,
+                                topic.type_name,
+                                2,
+                                1,
+                                {
+                                    10,
+                                    "Hz"
+                                }
+                            });
+
+    // Check information
+    ASSERT_EQ(result[0].name, expected_result[0].name);
+    ASSERT_EQ(result[0].type, expected_result[0].type);
+    ASSERT_EQ(result[0].datawriters, expected_result[0].datawriters);
+    ASSERT_EQ(result[0].datareaders, expected_result[0].datareaders);
 }
 
 int main(
