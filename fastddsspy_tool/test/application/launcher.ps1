e52e4011c4b45b11d12f2ec896a6db90ab5f161a
Irene Bandera <irenebandera@eprosima.com>
2023-03-27 08:40:09 +0200

    Add tool tests
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

---
+++ fastddsspy_tool/test/application/launcher.ps1
@@ -0,0 +1,34 @@
+Param(
+    [Parameter(Position=0, Mandatory=$true)]
+    [ValidateScript({Test-Path $_ -PathType Leaf -IsValid })]
+    [String]
+    # python3 binary
+    $python_path,
+
+    [Parameter(Position=1, Mandatory=$true)]
+    [ValidateScript({Test-Path $_ -PathType Leaf -IsValid })]
+    [String]
+    # python script that keeps the  testing
+    $test_script,
+
+    [Parameter(Position=2, Mandatory=$true)]
+    [ValidateScript({Test-Path $_ -PathType Leaf -IsValid })]
+    [String]
+    # fastddsspy creation binary full qualified path
+    $tool_path
+)
+
+$test = Start-Process -Passthru -Wait `
+    -FilePath $python_path `
+    -ArgumentList (
+        $test_script,
+        "--exe", $tool_path,
+        "--debug",
+        "--signal", "sigint") `
+    -WindowStyle Hidden
+
+if( $test.ExitCode -ne 0 )
+{
+    $error_message = "Test: $test_name failed with exit code $($test.ExitCode)."
+    throw $error_message
+}
