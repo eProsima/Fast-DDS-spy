27733c9c031f3b55225e9b63b3a5caf24ff68c3d
Irene Bandera <irenebandera@eprosima.com>
2023-03-28 09:24:48 +0200

    add commands
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

---
+++ fastddsspy_tool/test/application/datawriters.py
@@ -0,0 +1,48 @@
+# Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from utils import *
+
+
+if __name__ == '__main__':
+
+    args = parse_options()
+
+    # Create a custom logger
+    logger = logging.getLogger('SYS_TEST')
+    # Create handlers
+    l_handler = logging.StreamHandler()
+    # Create formatters and add it to handlers
+    l_format = '[%(asctime)s][%(name)s][%(levelname)s] %(message)s'
+    l_format = logging.Formatter(l_format)
+    l_handler.setFormatter(l_format)
+    # Add handlers to the logger
+    logger.addHandler(l_handler)
+    # Set log level
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    if args.exe is None:
+        logger.error(
+            'Executable binary file does not exist or has no '
+            'executable permissions.')
+        sys.exit(1)
+
+    spy = test_spy_start(args.exe, logger)
+
+    sys.exit(
+        test_spy_command(
+            spy, 'datawriters', logger))           # Path to executable
--- fastddsspy_tool/test/application/utils.py
+++ fastddsspy_tool/test/application/utils.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+# Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ USAGE = ('python3 tests.py -e <path/to/fastddsspy-executable>'
          ' [-d]')
 
 # Sleep time to let process init and finish
-SLEEP_TIME = 1
+SLEEP_TIME = 2
 
 
 def executable_permission_value():
@@ -86,7 +86,7 @@ def parse_options():
     return parser.parse_args()
 
 
-def test_spy_closure(fastddsspy):
+def test_spy_start(fastddsspy, logger):
 
     command = [fastddsspy]
     logger.info('Executing command: ' + str(command))
@@ -96,12 +96,13 @@ def test_spy_closure(fastddsspy):
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             universal_newlines=True)
+    return proc
 
-    # sleep to let the server run
-    time.sleep(SLEEP_TIME)
+
+def test_spy_command(proc, command, logger):
 
     try:
-        proc.communicate(input='exit', timeout=2)
+        proc.communicate(input=command, timeout=5)
         proc.kill()
         logger.debug('-----------------------------------------------------')
         logger.debug('Command ' + str(command) + ' worked.')
@@ -114,34 +115,3 @@ def test_spy_closure(fastddsspy):
         return 1
 
     return 0
-
-
-if __name__ == '__main__':
-
-    args = parse_options()
-
-    # Create a custom logger
-    logger = logging.getLogger('SYS_TEST')
-    # Create handlers
-    l_handler = logging.StreamHandler()
-    # Create formatters and add it to handlers
-    l_format = '[%(asctime)s][%(name)s][%(levelname)s] %(message)s'
-    l_format = logging.Formatter(l_format)
-    l_handler.setFormatter(l_format)
-    # Add handlers to the logger
-    logger.addHandler(l_handler)
-    # Set log level
-    if args.debug:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
-
-    if args.exe is None:
-        logger.error(
-            'Executable binary file does not exist or has no '
-            'executable permissions.')
-        sys.exit(1)
-
-    sys.exit(
-        test_spy_closure(
-            args.exe))           # Path to executable
