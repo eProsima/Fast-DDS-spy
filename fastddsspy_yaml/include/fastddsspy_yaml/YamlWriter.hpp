a8b94325bccb471230d3658bd78350139c14b13b
jparisu <javierparis@eprosima.com>
2023-04-04 12:21:46 +0200

    Add pragma once to headers
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- fastddsspy_yaml/include/fastddsspy_yaml/YamlWriter.hpp
+++ fastddsspy_yaml/include/fastddsspy_yaml/YamlWriter.hpp
@@ -12,5 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma once
+
 // Include implementation template file
 #include <fastddsspy_yaml/impl/YamlWriter.ipp>
