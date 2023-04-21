ef5ecf1ce34fc3246bb50f802319f142e2f5a030
Irene Bandera <irenebandera@eprosima.com>
2023-04-21 10:28:32 +0200

    Uncrustify and more timeout
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

--- fastddsspy_tool/test/application/test_class.py
+++ fastddsspy_tool/test/application/test_class.py
@@ -129,7 +129,7 @@ class TestCase():
         if (self.one_shot):
 
             try:
-                output = proc.communicate(timeout=10)[0]
+                output = proc.communicate(timeout=15)[0]
             except subprocess.TimeoutExpired:
                 proc.kill()
             if not self.valid_output(output):
@@ -209,7 +209,7 @@ class TestCase():
     def stop_tool(self, proc):
         """TODO."""
         try:
-            proc.communicate(input='exit\n', timeout=10)[0]
+            proc.communicate(input='exit\n', timeout=15)[0]
         except subprocess.TimeoutExpired:
             proc.kill()
 
@@ -217,7 +217,7 @@ class TestCase():
         """TODO."""
         try:
             proc.terminate()
-            proc.wait(timeout=10)
+            proc.wait(timeout=15)
         except subprocess.TimeoutExpired:
             proc.kill()
 
