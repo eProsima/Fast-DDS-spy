a31532290e680fb211821428209ce583b7de9bdc
tempate <danieldiaz@eprosima.com>
2024-04-09 09:35:21 +0200

    Fix python linter
    Signed-off-by: tempate <danieldiaz@eprosima.com>

--- fastddsspy_tool/test/application/test_cases/one_shot__domain_fail.py
+++ fastddsspy_tool/test/application/test_cases/one_shot__domain_fail.py
@@ -63,9 +63,8 @@ Debug parameters\n\
      --log-filter     Set a Regex Filter to filter by category the info and warning \
 log entries. [Default = "FASTDDSSPY"]. \n\
      --log-verbosity  Set a Log Verbosity Level higher or equal the one given. \
-(Values accepted: "info","warning","error" no Case Sensitive) [Default = "warning"]. \n\n
-"""
-)
+(Values accepted: "info","warning","error" no Case Sensitive) [Default = "warning"]. \n\n"""
+        )
 
     def valid_output(self, output):
         """
