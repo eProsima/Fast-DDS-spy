0ec408d66e236a57f0c950bc08304d4642dcef4d
Denisa <denisa@eprosima.com>
2025-05-21 19:31:17 +0200

    Fix linter 2
    Signed-off-by: Denisa <denisa@eprosima.com>

--- fastddsspy_tool/test/application/test_cases/tool_help_dds.py
+++ fastddsspy_tool/test/application/test_cases/tool_help_dds.py
@@ -40,49 +40,48 @@ configuration_discovery_time.yaml',
             arguments_dds=[],
             arguments_spy=['--config-path', 'configuration'],
             commands_spy=['help'],
-            output = (
-    "Fast DDS Spy is an interactive CLI that allow to instrospect DDS networks.\n\n"
-    "Each command shows data related with the network in Yaml format.\n\n"
-    "Commands available and the information they show:\n\n"
-    "\thelp                            : this help.\n\n"
-    "\tversion                         : tool version.\n\n"
-    "\tquit                            : exit interactive CLI and close program.\n\n"
-    "\tparticipants                    : DomainParticipants discovered in the network.\n\n"
-    "\tparticipants verbose            : verbose information about DomainParticipants "
-    "discovered in the network.\n\n"
-    "\tparticipants <Guid>             : verbose information related with a specific "
-    "DomainParticipant.\n\n"
-    "\twriters                         : DataWriters discovered in the network.\n\n"
-    "\twriters verbose                 : verbose information about DataWriters discovered "
-    "in the network.\n\n"
-    "\twriters <Guid>                  : verbose information related with a specific "
-    "DataWriter.\n\n"
-    "\treader                          : DataReaders discovered in the network.\n\n"
-    "\treader verbose                  : verbose information about DataReaders discovered "
-    "in the network.\n\n"
-    "\treader <Guid>                   : verbose information related with a specific "
-    "DataReader.\n\n"
-    "\ttopics                          : Topics discovered in the network in compact format.\n\n"
-    "\ttopics v                        : Topics discovered in the network.\n\n"
-    "\ttopics vv                       : verbose information about Topics discovered "
-    "in the network.\n\n"
-    "\ttopics <name>                   : Topics discovered in the network filtered by name "
-    "(wildcard allowed (*)).\n\n"
-    "\techo <name>                     : data of a specific Topic (Data Type must be discovered).\n\n"
-    "\techo <wildcard_name>            : data of Topics matching the wildcard name "
-    "(and whose Data Type is discovered).\n\n"
-    "\techo <name> verbose             : data with additional source info of a specific Topic.\n\n"
-    "\techo <wildcard_name> verbose    : data with additional source info of Topics matching the "
-    "topic name (wildcard allowed (*)).\n\n"
-    "\techo all                        : verbose data of all topics (only those whose "
-    "Data Type is discovered).\n\n"
-    "\n\n"
-    "Notes and comments:\n\n"
-    "\tTo exit from data printing, press enter.\n\n"
-    "\tEach command is accessible by using its first letter (h/v/q/p/w/r/t/s).\n\n"
-    "\n\n"
-    "For more information about these commands and formats, please refer to the documentation:\n\n"
-    "https://fast-dds-spy.readthedocs.io/en/latest/\n"
-    )
-
+            output=(
+                'Fast DDS Spy is an interactive CLI that allow to instrospect DDS networks.\n\n'
+                'Each command shows data related with the network in Yaml format.\n\n'
+                'Commands available and the information they show:\n\n'
+                '\thelp                            : this help.\n\n'
+                '\tversion                         : tool version.\n\n'
+                '\tquit                            : exit interactive CLI and close program.\n\n'
+                '\tparticipants                    : DomainParticipants discovered in the network.\n\n'
+                '\tparticipants verbose            : verbose information about DomainParticipants '
+                'discovered in the network.\n\n'
+                '\tparticipants <Guid>             : verbose information related with a specific '
+                'DomainParticipant.\n\n'
+                '\twriters                         : DataWriters discovered in the network.\n\n'
+                '\twriters verbose                 : verbose information about DataWriters discovered '
+                'in the network.\n\n'
+                '\twriters <Guid>                  : verbose information related with a specific '
+                'DataWriter.\n\n'
+                '\treader                          : DataReaders discovered in the network.\n\n'
+                '\treader verbose                  : verbose information about DataReaders discovered '
+                'in the network.\n\n'
+                '\treader <Guid>                   : verbose information related with a specific '
+                'DataReader.\n\n'
+                '\ttopics                          : Topics discovered in the network in compact format.\n\n'
+                '\ttopics v                        : Topics discovered in the network.\n\n'
+                '\ttopics vv                       : verbose information about Topics discovered '
+                'in the network.\n\n'
+                '\ttopics <name>                   : Topics discovered in the network filtered by name '
+                '(wildcard allowed (*)).\n\n'
+                '\techo <name>                     : data of a specific Topic (Data Type must be discovered).\n\n'
+                '\techo <wildcard_name>            : data of Topics matching the wildcard name '
+                '(and whose Data Type is discovered).\n\n'
+                '\techo <name> verbose             : data with additional source info of a specific Topic.\n\n'
+                '\techo <wildcard_name> verbose    : data with additional source info of Topics matching the '
+                'topic name (wildcard allowed (*)).\n\n'
+                '\techo all                        : verbose data of all topics (only those whose '
+                'Data Type is discovered).\n\n'
+                '\n\n'
+                'Notes and comments:\n\n'
+                '\tTo exit from data printing, press enter.\n\n'
+                '\tEach command is accessible by using its first letter (h/v/q/p/w/r/t/s).\n\n'
+                '\n\n'
+                'For more information about these commands and formats, please refer to the documentation:\n\n'
+                'https://fast-dds-spy.readthedocs.io/en/latest/\n'
+            )
         )
