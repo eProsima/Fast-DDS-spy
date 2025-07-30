e50d0137ba46d37b8406dfd67ba81eb5f409081e
Denisa <denisa@eprosima.com>
2025-07-30 16:04:19 +0200

    Fix uncrustify 2
    Signed-off-by: Denisa <denisa@eprosima.com>

--- fastddsspy_tool/src/cpp/tool/Controller.cpp
+++ fastddsspy_tool/src/cpp/tool/Controller.cpp
@@ -545,7 +545,7 @@ void Controller::help_command_(
             << "\tquit                            : exit interactive CLI and close program.\n"
             << "\tparticipants                    : DomainParticipants discovered in the network.\n"
             <<
-                    "\tparticipants verbose            : verbose information about DomainParticipants discovered in the network.\n"
+            "\tparticipants verbose            : verbose information about DomainParticipants discovered in the network.\n"
             << "\tparticipants <Guid>             : verbose information related with a specific DomainParticipant.\n"
             << "\twriters                         : DataWriters discovered in the network.\n"
             << "\twriters verbose                 : verbose information about DataWriters discovered in the network.\n"
@@ -557,17 +557,17 @@ void Controller::help_command_(
             << "\ttopics v                        : Topics discovered in the network.\n"
             << "\ttopics vv                       : verbose information about Topics discovered in the network.\n"
             <<
-                    "\ttopics <name>                   : Topics discovered in the network filtered by name (wildcard allowed (*)).\n"
+            "\ttopics <name>                   : Topics discovered in the network filtered by name (wildcard allowed (*)).\n"
             <<
-                    "\ttopics <name> idl               : Display the IDL type definition for topics matching <name> (wildcards allowed).\n"
+            "\ttopics <name> idl               : Display the IDL type definition for topics matching <name> (wildcards allowed).\n"
             << "\techo <name>                     : data of a specific Topic (Data Type must be discovered).\n"
             <<
-                    "\techo <wildcard_name>            : data of Topics matching the wildcard name (and whose Data Type is discovered).\n"
+            "\techo <wildcard_name>            : data of Topics matching the wildcard name (and whose Data Type is discovered).\n"
             << "\techo <name> verbose             : data with additional source info of a specific Topic.\n"
             <<
-                    "\techo <wildcard_name> verbose    : data with additional source info of Topics matching the topic name (wildcard allowed (*)).\n"
+            "\techo <wildcard_name> verbose    : data with additional source info of Topics matching the topic name (wildcard allowed (*)).\n"
             <<
-                    "\techo all                        : verbose data of all topics (only those whose Data Type is discovered).\n"
+            "\techo all                        : verbose data of all topics (only those whose Data Type is discovered).\n"
             << "\n"
             << "Notes and comments:\n"
             << "\tTo exit from data printing, press enter.\n"
