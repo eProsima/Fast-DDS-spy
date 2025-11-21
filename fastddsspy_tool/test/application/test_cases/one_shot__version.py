8d57e1af71e400df92b485b9ab13a5ee832906da
Carlos Ferreira González <carlosferreira@eprosima.com>
2025-11-21 11:47:01 +0100

    Bump to v1.4.0 (#136)
    * Refs #23927: Bump to v1.4.0
    
    Signed-off-by: Carlos Ferreira González <carlosferreira@eprosima.com>
    
    * Refs #23927: Revision
    
    Signed-off-by: Carlos Ferreira González <carlosferreira@eprosima.com>
    
    ---------
    
    Signed-off-by: Carlos Ferreira González <carlosferreira@eprosima.com>

--- fastddsspy_tool/test/application/test_cases/one_shot__version.py
+++ fastddsspy_tool/test/application/test_cases/one_shot__version.py
@@ -36,7 +36,7 @@ class TestCase_instance (test_class.TestCase):
             arguments_dds=[],
             arguments_spy=['--version'],
             commands_spy=[],
-            output="""Fast DDS Spy v1.3.0\
+            output="""Fast DDS Spy v1.4.0\
 commit hash: 16ed7e8c93d7481d8b426746af9ec3ffa323f451\n"""
         )
 
