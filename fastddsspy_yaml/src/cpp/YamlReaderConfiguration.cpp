faaa92a5bbe0b66c1d6eadcfb46e80b5164d0321
DannyP39 <98972125+Danipiza@users.noreply.github.com>
2026-06-02 09:16:33 +0200

    Fix macOS build issues in YAML parsing and CLI arguments (#150)
    Signed-off-by: danipiza <dpizarrogallego@gmail.com>

--- fastddsspy_yaml/src/cpp/YamlReaderConfiguration.cpp
+++ fastddsspy_yaml/src/cpp/YamlReaderConfiguration.cpp
@@ -186,11 +186,9 @@ void Configuration::load_dds_configuration_(
 
         for (const auto& topic: manual_topics)
         {
-            const std::string key(topic.first->topic_name);
+            const std::string key(topic.first->topic_name.get_reference());
 
-            // force an unambiguous std::string
-            const std::string filter =
-                    static_cast<std::string>(topic.first->content_topic_filter);
+            const std::string filter = topic.first->content_topic_filter.get_reference();
 
             auto ret = dds_configuration->content_topic_filter_dict.insert(
                 std::make_pair(key, filter)
