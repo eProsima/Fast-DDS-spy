04fcba7081481e51425a2d2c7793133a1b9cc4b3
danipiza <dpizarrogallego@gmail.com>
2026-07-14 08:03:18 +0200

    [#25501] Added missing tests
    Signed-off-by: danipiza <dpizarrogallego@gmail.com>

---
+++ fastddsspy_tool/test/application/test_cases/tool_datawriter_dds_partition_match_filter.py
@@ -0,0 +1,53 @@
+# Copyright 2026 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for the fastddsspy executable."""
+
+import test_class
+
+
+class TestCase_instance (test_class.TestCase):
+    """@brief A subclass of `test_class.TestCase` representing a specific test case."""
+
+    def __init__(self):
+        """
+        @brief Initialize the TestCase_instance object.
+
+        The publisher uses a non-empty partition ("A") and the filter partition
+        ("A") matches it, so the writer passes the filter and is printed.
+        Exercises the non-empty partition-matching branch of partitions_match.
+
+        This test launch:
+            fastddsspy --config-path fastddsspy_tool/test/application/configuration/\
+                configuration_discovery_time.yaml
+            >> filter add partitions A
+            >> datawriter
+            AdvancedConfigurationExample publisher --partitions=A
+        """
+        super().__init__(
+            name='ToolDatawriterDDSPartitionMatchFilter',
+            one_shot=False,
+            command=[],
+            dds=True,
+            config='fastddsspy_tool/test/application/configuration/\
+configuration_discovery_time.yaml',
+            arguments_dds=['--partitions=A'],
+            arguments_spy=['--config-path', 'configuration'],
+            commands_spy=['filter add partitions A', 'datawriter'],
+            output="""- guid: %%guid%%\n\
+\n\
+  participant: Participant_pub\n\
+\n\
+  topic: HelloWorldTopic [HelloWorld]\n"""
+        )
